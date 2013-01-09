46526408b90bf783d832b98c8bf826699457269b
Chris Bednarski <banzaimonkey@gmail.com>
2013-01-09 01:57:08 -0800

    Added flyweight with skeleton implementation

---
+++ flyweight.py
@@ -0,0 +1,28 @@
+import subprocess
+import shlex
+
+class git:
+    def clone(source, target):
+        pass
+
+    def fetch(path):
+        pass
+
+    def checkout(revision):
+        pass
+
+    def getTags(path):
+        pass
+
+class flyweight:
+    def updateRepos():
+        pass
+
+    def updateCDN():
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
